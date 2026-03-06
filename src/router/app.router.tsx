16a463700a5799b32f44bef73bc61533c6a80f65
Heissen Lopez <heissen.lopez@gmail.com>
2026-03-06 14:18:50 -0500

    Add intial Layout and use in routes

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,24 +1,40 @@
+import { createBrowserRouter } from 'react-router';
+
+import { AdminLayout } from '@/admin/layouts/AdminLayout';
 import { AdminPage } from '@/admin/pages/AdminPage';
+import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
 import { HeroPage } from '@/heroes/pages/hero/pages/HeroPage';
 import { HomePage } from '@/heroes/pages/home/HomePage';
 import { SearchPage } from '@/heroes/pages/search/SearchPage';
-import { createBrowserRouter } from 'react-router';
 
 export const appRouter = createBrowserRouter([
   {
-    path: '/admin',
-    element: <AdminPage />
-  },
-  {
     path: '/',
-    element: <HomePage />
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'heroes/1',
+        element: <HeroPage />
+      },
+      {
+        path: 'search',
+        element: <SearchPage />
+      },
+    ]
   },
   {
-    path: 'heroes/1',
-    element: <HeroPage />
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />
+      }
+    ]
   },
-  {
-    path: '/search',
-    element: <SearchPage />
-  }
+
 ]);
