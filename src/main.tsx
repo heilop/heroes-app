52b208050139234e1e85b8a62d0b9c122fdb0c74
Heissen Lopez <heissen.lopez@gmail.com>
2026-03-06 12:31:38 -0500

    Initial commit
    Add shadcdn, tailwind and initial configs

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { Button } from './components/ui/button'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <h1>Hello World!</h1>
+
+    <Button>Hello Again!</Button>
+  </StrictMode>,
+)
