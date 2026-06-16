008d78ee2a5b5509e36dd59701e58fe9e5f10455
Heissen Lopez <heissen.lopez@gmail.com>
2026-06-15 22:11:12 -0500

    Add heroAction tests and updated comp

--- src/heroes/actions/get-hero.action.ts
+++ src/heroes/actions/get-hero.action.ts
@@ -6,7 +6,6 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 export const getHeroAction = async (idSlug: string) => {
   const { data } = await heroApi.get<Hero>(`/${idSlug}`);
 
-  console.log({ data });
   return {
     ...data,
     image: `${BASE_URL}/images/${data.image}`,
